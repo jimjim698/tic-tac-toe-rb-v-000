31e1a44c9a80ba57cb5360c2ec81ac6320e715c8
James Lotruglio <jimlotruglio@yahoo.com>
2017-11-30 02:09:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -121,11 +121,11 @@ end
 def play(board)
   until over?(board)
   turn(board)
+end
 if won?(board)
     puts "Congratulations #{winner(board)}!"
 elsif draw?(board)
 puts "Cat's Game!"
 
-   end
   end
 end
